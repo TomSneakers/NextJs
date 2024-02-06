317961e9ed3c1f76fe0aa4822f86feaac7d0e42f
Tom <tomdesvignes031@gmail.com>
2024-02-06 11:15:14 +0100

    building

--- 4/nextjs/app/ui/customers/table.tsx
+++ 4/nextjs/app/ui/customers/table.tsx
@@ -16,8 +16,6 @@ export default async function CustomersTable({
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
         Customers
       </h1>
-      import Search from '@/app/ui/search';
-
       <Search/>
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
