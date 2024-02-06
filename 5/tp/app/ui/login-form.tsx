a3615e6a350a5f45c1c27521b12d3466f6a19ee6
Tom <tomdesvignes031@gmail.com>
2024-02-06 16:55:32 +0100

    start to Home s Dashboard

--- 5/tp/app/ui/login-form.tsx
+++ 5/tp/app/ui/login-form.tsx
@@ -16,7 +16,7 @@ export default function LoginForm() {
 
   return (
     <form action={dispatch} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+      <div className="flex-1 rounded-lg bg-white px-6 pb-4 pt-8">
         <h1 className={`${noto_sans.className} mb-3 text-2xl text-center`}>
           Connexion
         </h1>
