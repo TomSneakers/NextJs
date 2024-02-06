0ce3e48155cd5cb0bccb4378d22cb0df37843980
Tom <tomdesvignes031@gmail.com>
2024-02-06 10:50:53 +0100

    maj

---
+++ 4/nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import { lusitana } from '../../ui/fonts';
+export default function Page() {
+    return (
+        <main className="flex min-h-screen flex-col p-6">
+        <p className={`${lusitana.className}`}>Customers Page</p>
+        </main>
+    );
+    }
